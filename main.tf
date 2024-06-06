3768bfc3595526f247cd1dd620a54e34b7cdf0ef
Manasa Bhat <manasa.bhat@kyndryl.com>
2024-06-06 13:11:01 +0530

    apply

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mana1" {
-  ami           = "ami-04b70fa74e45c3917"                 
+  ami           = "ami-09040d770ffe2224f"                 
  instance_type = var.instance_type 
   tags = {
     Name = "mana"
