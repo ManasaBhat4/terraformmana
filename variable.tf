c0519cc84a51f0575da1d7dd2bd1e2375dc21741
Manasa Bhat <manasa.bhat@kyndryl.com>
2024-06-06 12:19:43 +0530

    apply

--- variable.tf
+++ variable.tf
@@ -2,3 +2,7 @@ variable "instance_type" {
   description = "The type of EC2 instance to launch."
   default     = "t2.micro"
 }
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "mana-bucket"
+}  
